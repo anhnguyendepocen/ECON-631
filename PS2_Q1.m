82f04b8141b15d0ac868122c0adf2d752be247b5
Russell Morton <russell.morton@gmail.com>
2019-10-18 17:27:35 -0400

    Updated and complete.  BUT producer surplus decreases post-merger, which I think is problematic...

--- PS2_Q1.m
+++ PS2_Q1.m
@@ -62,6 +62,28 @@ weight_next = .05;
 
 pricesq2 = priceitercomp(alpha2,beta2,sigma2,x,mc,prices_curr,norm_rnd,tol,weight_next);
 
+
+%%
+%for intuition: try with just sigma down.
+%Higher sigma-more taste diff - more market power
+
+pricesq2b_sigma = priceitercomp(alpha,beta,sigma2,x,mc,pricesq2,norm_rnd,tol,weight_next);
+%so this LOWERS Price, consistent with above
+
+pricesq2b_alpha = priceitercomp(alpha2,beta,sigma,x,mc,pricesq2,norm_rnd,tol,weight_next);
+%so this INCREASES price, consistent with less price sensitive consumers
+
+pricesq2b_beta = priceitercomp(alpha,beta2,sigma,x,mc,pricesq2,norm_rnd,tol,weight_next);
+%everyone raised prices? consumers MORE price elastic (to switch to outside)
+%more elastic-lower prices
+
+prices_beta_test = zeros(50,4);
+for i = 1:50;
+       beta_test = beta1 - i * (1/100);
+       prices_beta_test(i,1) = beta_test;
+       prices_beta_test_temp = priceitercomp(alpha,beta_test,sigma,x,mc,pricesq2,norm_rnd,tol,weight_next);
+       prices_beta_test(i,2:4) =  prices_beta_test_temp;
+end;
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%
 %   Question 3: Merger Price Effects
@@ -80,8 +102,51 @@ pricesq3 = priceitermerge(alpha2,beta2,sigma2,x,mc,prices_curr,norm_rnd,tol,weig
 %   Question 4: Merger Effects on Welfare
 %%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
+%%
+%Calc CS: find logit inclusive values for each case
+    random_coeffs = sigma * x' .* ones(rows(x'),rows(norm_rnd)) .* norm_rnd';
+   
+   %premerger
+    pre_logit_incl_premerger_sim = exp( (beta * x - alpha * pricesq2)' .* ones(1,rows(norm_rnd))...
+                    + random_coeffs );
+    mean_logit_incl_premerger_sim = mean(pre_logit_incl_premerger_sim,2);
+    logit_incl_premerger = log(sum(mean_logit_incl_premerger_sim));
+    
+    %postmerger
+    pre_logit_incl_postmerger_sim = exp( (beta * x - alpha * pricesq3)' .* ones(1,rows(norm_rnd))...
+                    + random_coeffs );
+    mean_logit_incl_postmerger_sim = mean(pre_logit_incl_postmerger_sim,2);
+    logit_incl_postmerger = log(sum(mean_logit_incl_postmerger_sim));
+    
+    cs_change = (1 / alpha2) * (logit_incl_postmerger - logit_incl_premerger);
+    
+%%
+%Calc Producer Surplus
+    %premerger
+    s_curr_num_sim = exp( (beta * x - alpha * pricesq2)' .* ones(1,rows(norm_rnd))...
+                    + random_coeffs );
+    sum_s_curr_num_sim = sum(s_curr_num_sim);
+    pre_s_curr_denom_sim = (1 + sum_s_curr_num_sim);
+    s_curr_denom_sim = horzcat(pre_s_curr_denom_sim',pre_s_curr_denom_sim',pre_s_curr_denom_sim')';
+    s_curr_sim = s_curr_num_sim ./ s_curr_denom_sim;
+    s_curr = mean(s_curr_sim,2);
+    share_premerger = s_curr;
+    
+    pre_producer_premerger = share_premerger .* (pricesq2' - mc');
+    producer_premerger = sum(pre_producer_premerger);
+    
+    %postmerger
+    s_curr_num_sim = exp( (beta * x - alpha * pricesq3)' .* ones(1,rows(norm_rnd))...
+                    + random_coeffs );
+    sum_s_curr_num_sim = sum(s_curr_num_sim);
+    pre_s_curr_denom_sim = (1 + sum_s_curr_num_sim);
+    s_curr_denom_sim = horzcat(pre_s_curr_denom_sim',pre_s_curr_denom_sim',pre_s_curr_denom_sim')';
+    s_curr_sim = s_curr_num_sim ./ s_curr_denom_sim;
+    s_curr = mean(s_curr_sim,2);
+    share_postmerger = s_curr;
+    
+    pre_producer_postmerger = share_postmerger .* (pricesq3' - mc');    
+    producer_postmerger = sum(pre_producer_postmerger);
 
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%
